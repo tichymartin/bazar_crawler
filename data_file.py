709a13bd097ec8188011ce2f1b1fe84af7b7d3a1
tichymartin <tichymartin@email.cz>
2019-02-11 15:39:07 +0100

    add annonce

--- data_file.py
+++ data_file.py
@@ -2,6 +2,7 @@ links_dict = {
     "sbazar": [r"524-jidelni-stoly-zidle", r"300-kresla", r"219-starozitny-nabytek", ],
     "bazos": ["nabytek.bazos.cz/kresla", "nabytek.bazos.cz/zidle", "ostatni.bazos.cz", ],
     "letgo": ["%C5%BEidle ", "k%C5%99esla ", ],
+    "annonce": ["kresla-gauce", "zidle-byt-kancelar", ],
 
 }
 

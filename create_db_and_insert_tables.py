9847d45a030fedf2e9f2f23f2ca388380ad0edc1
tichymartin <tichymartin@email.cz>
2021-12-01 13:31:34 +0100

    rise of phoenix

--- create_db_and_insert_tables.py
+++ create_db_and_insert_tables.py
@@ -1,5 +1,8 @@
 from general import create_database_file, delete_database_file
 
 if __name__ == '__main__':
-    delete_database_file()
+    try:
+        delete_database_file()
+    except:
+        pass
     create_database_file()

b23b69d8d342d5aa3b757f034fe5007973b30393
tichymartin <tichymartin@email.cz>
2021-12-07 10:47:29 +0100

    oprava importu

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
-from main import get_links_from_website, check_links_in_database, insert_new_links_into_database, \
-    search_links_for_metadata
-from send_mail import send_mails
+from main import get_links_from_website, check_links_in_database
 from general import start_logger, stop_logger
 from data_file import servers_to_search
 from one_link_process import link_process
@@ -10,7 +8,6 @@ if __name__ == '__main__':
 
     new_links = get_links_from_website(servers_to_search)
     links_to_check = check_links_in_database(new_links)
-
     link_process(links_to_check)
 
     stop_logger(logger, timer)
